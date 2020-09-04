b1758d6fdce11baf5b3f3bb483c1dcb288116c27
Preston Davis <prestonseandavis101@yahoo.com>
2020-09-04 01:55:58 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,13 +3,18 @@ require 'pry'
 class School
   
   attr_accessor :roster
+  
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+  
+  attr_accessor :roster
   def initialize(school_name)
     @school_name = school_name
    @roster = {}
   end
 
   def add_student(student_name, level)
-    if roster[level]==nil
+    if roster[level] == nil
      binding.pry
-end
-end
