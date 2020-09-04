5a5603a726cdcd36e576aa645db8f4d21bcd4c3b
Preston Davis <prestonseandavis101@yahoo.com>
2020-09-04 01:09:44 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,8 +7,8 @@ class School
   end
   
  def add_student(student_name, grade)
-@roster[grade]=[student_name]
-@roster[grade]<<[new_student_name]
+@roster[grade]=[student_name1]
+@roster[grade]<<[student_name2]
  end
  
  def school_name
