6f381eac9043ca9487600f4e9a699513b44c9423
Preston Davis <prestonseandavis101@yahoo.com>
2020-09-04 01:57:20 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,12 +9,9 @@ class School
     @roster = {}
   end
   
-  attr_accessor :roster
-  def initialize(school_name)
-    @school_name = school_name
-   @roster = {}
-  end
-
   def add_student(student_name, level)
     if roster[level] == nil
-     binding.pry
+      binding.pry
+    end
+  end
+end
